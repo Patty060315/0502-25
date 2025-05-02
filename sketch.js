152255ba6dcd2b2e1f6ad3a05ab1cf5023823ba3
Patty060315 <413730176@o365.tku.edu.tw>
2025-05-02 10:14:22 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let capture;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight); // 設定畫布為全螢幕
   background('#efcfe3'); // 設定背景顏色為 efcfe3
 
   // 初始化攝影機擷取
@@ -17,4 +17,9 @@ function draw() {
   let x = (width - capture.width) / 2;
   let y = (height - capture.height) / 2;
   image(capture, x, y, capture.width, capture.height);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight); // 當視窗大小改變時調整畫布大小
+  capture.size(width * 0.8, height * 0.8); // 重新設定影像大小
 }
